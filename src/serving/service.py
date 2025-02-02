6e5626b4600723efef0e52811dc04c2eeaf432d9
eunji yeom <mykiringirin@gmail.com>
2025-02-02 20:45:15 +0900

    PR#5 정적 분석으로 탐지된 위험 코드 제거

--- src/serving/service.py
+++ src/serving/service.py
@@ -19,7 +19,7 @@ with bentoml.importing():
 # Define a BentoML service with specified resource limits and traffic settings
 @bentoml.service(
     resources={"cpu": "4"},
-    traffic={"timeout": 30},
+    traffic={"timeout": 300},
 )
 class ModelService:
     """
@@ -37,7 +37,9 @@ class ModelService:
         """
         Initialize the BentoML service.
         """
-        self.data_path = load_yaml("data")["data"]["dataset_path"]
+        data_conf = load_yaml("data")["data"]
+        self.data_path = data_conf["dataset_path"]
+        self.num_workers = data_conf["num_workers"]
 
     @bentoml.api(route="/run_model")
     def run_model(self, model_type: str) -> str:
@@ -50,7 +52,11 @@ class ModelService:
         Returns:
             str: The evaluation results of the model.
         """
-        print('[STEP 0] start')
+        self.model_type = model_type
+        self.conf = load_yaml(self.model_type)
+        
+        print(f"[Model Type] {self.model_type}")
+        print("[STEP 0] start")
 
         print("[STEP 1] load data")
         data = load_data(self.data_path)
@@ -58,20 +64,20 @@ class ModelService:
         print("[STEP 2] preprocess data")
         X, y, num_cols, cat_cols = preprocess_data(data)
 
-        if model_type == 'lgbm':
+        if self.model_type == "lgbm":
             """
             Run the LightGBM model.
             """
             print("[STEP 3] Running LightGBM model")
-            X_train, X_test, y_train, y_test = split_data(
+            x_train, x_test, y_train, y_test = split_data(
                 X, y, num_cols, cat_cols, split_num_cat=False
             )
             model = LightGBMModel()
-            model.fit(X_train, X_test, y_train, y_test, cat_cols)
-            evaluation = evaluate_lgbm_model(model, X_test, y_test)
-            bento_model = bentoml.lightgbm.save_model(self.model_type, model.lgbm_model)
+            model.fit(x_train, x_test, y_train, y_test, cat_cols)
+            eval = evaluate_lgbm_model(model, x_test, y_test)
+            bento_model = bentoml.lightgbm.save_model(name=self.model_type, model=model.lgbm_model)
 
-            return f"Model tag : {bento_model.tag} / LightGBM evaluation: {evaluation}"
+            return f"Model tag : {bento_model.tag} / LightGBM evaluation: {eval}"
 
         elif model_type == "deepfm":
             """
@@ -82,76 +88,89 @@ class ModelService:
                 split_data(X, y, num_cols, cat_cols, split_num_cat=True)
             )
 
-            num_numeric = len(num_cols)
-            categorical_dims = [X[col].nunique() for col in cat_cols]
-            embedding_dim = self.conf["model"]["embedding_dim"]
-            deep_dims = self.conf["model"]["deep_dims"]
-            dropout = self.conf["model"]["dropout"]
-            batch_size = self.conf["training"]["batch_size"]
-            epochs = self.conf["training"]["epochs"]
-            lr = self.conf["training"]["learning_rate"]
-
             train_dataset = CriteoDataset(X_train_num, X_train_cat, y_train)
             test_dataset = CriteoDataset(X_test_num, X_test_cat, y_test)
 
             train_loader = DataLoader(
-                train_dataset, batch_size=batch_size, shuffle=True
+                train_dataset,
+                batch_size=self.conf["training"]["batch_size"],
+                shuffle=True,
+                num_workers=self.num_workers,
             )
-            test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+            test_loader = DataLoader(
+                test_dataset,
+                batch_size=self.conf["training"]["batch_size"],
+                shuffle=False,
+                num_workers=self.num_workers,
+            )
 
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
             model = DeepFM(
-                num_numeric, categorical_dims, embedding_dim, deep_dims, dropout
+                num_numeric=len(num_cols),
+                categorical_dims=[X[col].nunique() for col in cat_cols],
+                embedding_dim=self.conf["model"]["embedding_dim"],
+                deep_dims=self.conf["model"]["deep_dims"],
+                dropout=self.conf["model"]["dropout"],
             ).to(device)
-            train_dl_model(model, train_loader, test_loader, epochs, lr)
 
-            evaluation = evaluate_dl_model(model, test_loader)
-            bento_model = bentoml.pytorch.save_model(self.model_type, model)
+            train_dl_model(
+                model=model,
+                train_loader=train_loader,
+                test_loader=test_loader,
+                epochs=self.conf["training"]["epochs"],
+                lr=self.conf["training"]["learning_rate"],
+            )
+            eval = evaluate_dl_model(model=model, dataloader=test_loader)
+            bento_model = bentoml.pytorch.save_model(name=self.model_type, model=model)
 
-            return f"Model tag : {bento_model.tag} / DeepFM evaluation: {evaluation}"
+            return f"Model tag : {bento_model.tag} / DeepFM evaluation: {eval}"
 
         elif model_type == "dcn_v2":
             """
             Run the DCNv2 model.
             """
             print("[STEP 3] Running DCNv2 model")
-            X_train_num, X_test_num, X_train_cat, X_test_cat, y_train, y_test = (
+            x_train_num, x_test_num, x_train_cat, x_test_cat, y_train, y_test = (
                 split_data(X, y, num_cols, cat_cols, split_num_cat=True)
             )
 
-            num_numeric = len(num_cols)
-            categorical_dims = [X[col].nunique() for col in cat_cols]
-            embedding_dim = self.conf["model"]["embedding_dim"]
-            deep_dims = self.conf["model"]["deep_dims"]
-            cross_layers = self.conf["model"]["cross_layers"]
-            dropout = self.conf["model"]["dropout"]
-            batch_size = self.conf["training"]["batch_size"]
-            epochs = self.conf["training"]["epochs"]
-            lr = self.conf["training"]["learning_rate"]
-
-            train_dataset = CriteoDataset(X_train_num, X_train_cat, y_train)
-            test_dataset = CriteoDataset(X_test_num, X_test_cat, y_test)
+            train_dataset = CriteoDataset(x_train_num, x_train_cat, y_train)
+            test_dataset = CriteoDataset(x_test_num, x_test_cat, y_test)
 
             train_loader = DataLoader(
-                train_dataset, batch_size=batch_size, shuffle=True
+                train_dataset,
+                batch_size=self.conf["training"]["batch_size"],
+                shuffle=True,
+                num_workers=self.num_workers,
             )
-            test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+            test_loader = DataLoader(
+                test_dataset,
+                batch_size=self.conf["training"]["batch_size"],
+                shuffle=False,
+                num_workers=self.num_workers,
+            )
 
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
             model = DCNv2(
-                num_numeric,
-                categorical_dims,
-                embedding_dim,
-                deep_dims,
-                cross_layers,
-                dropout,
+                num_numeric=len(num_cols),
+                categorical_dims=[X[col].nunique() for col in cat_cols],
+                embedding_dim=self.conf["model"]["embedding_dim"],
+                deep_dims=self.conf["model"]["deep_dims"],
+                num_cross_layers=self.conf["model"]["cross_layers"],
+                dropout=self.conf["model"]["dropout"],
             ).to(device)
-            train_dl_model(model, train_loader, test_loader, epochs, lr)
 
-            evaluation = evaluate_dl_model(model, test_loader)
-            bento_model = bentoml.pytorch.save_model(self.model_type, model)
+            train_dl_model(
+                model=model,
+                train_loader=train_loader,
+                test_loader=test_loader,
+                epochs=self.conf["training"]["epochs"],
+                lr=self.conf["training"]["learning_rate"],
+            )
+            eval = evaluate_dl_model(model=model, dataloader=test_loader)
+            bento_model = bentoml.pytorch.save_model(name=self.model_type, model=model)
 
-            return f"Model tag : {bento_model.tag} / DCNv2 evaluation: {evaluation}"
+            return f"Model tag : {bento_model.tag} / DCNv2 evaluation: {eval}"
 
         else:
             return "Invalid model type. Please choose from ['lgbm', 'deepfm', 'dcn_v2']"
